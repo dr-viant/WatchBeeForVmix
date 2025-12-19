01be86c645aefd4298937a4e2763bd7033f4f939
Victor Ott Anthony <viant@dr.dk>
2025-12-19 07:51:03 +0000

    Changed destination of windows build

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,9 @@
-import { describe, it, expect, beforeEach, afterEach } from 'vitest';
+import { describe, it, expect, beforeEach, afterEach, test } from 'vitest';
 import os from 'os';
 import path from 'path';
-import fs from 'fs/promises';
+import { promises as fs } from 'fs';
+import { readFile } from "fs/promises"
+import { writeFile } from "fs/promises"
 import nock from 'nock';
 
 // CommonJS modul, så vi bruger require her:
@@ -12,12 +14,13 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 describe('watcher integration med vMix API', () => {
   let dir;
   let watcherHandle;
-
+  let config;
   const baseUrl = 'http://localhost:8088';
 
   beforeEach(async () => {
     // Opret en isoleret temp-mappe til denne test
     dir = await fs.mkdtemp(path.join(os.tmpdir(), 'watcher-test-'));
+    const configPath = path.join(dir, 'config.json');
 
     // Blokér rigtige netværkskald – alt skal via nock
     nock.cleanAll();
@@ -28,6 +31,12 @@ describe('watcher integration med vMix API', () => {
       vmixUrl: baseUrl,
       supportedExtensions: ['.mp4', '.mov', '.wmv', '.avi', '.mpg', '.mpeg'],
     };
+  
+    writeFile(
+      configPath,
+      JSON.stringify(config, null, 2),
+      'utf8',
+    );
 
     watcherHandle = startWatcher(config);
   });
@@ -52,6 +61,9 @@ describe('watcher integration med vMix API', () => {
     const encodedPath = encodeURIComponent(absolutePath);
     const inputName = path.basename(path.dirname(absolutePath));
 
+    // Act: Opret filen
+    await fs.writeFile(filePath, 'mockdata');
+
     // Forvent, at din kode laver et GET til dette endpoint
     nock(baseUrl)
       .get(
@@ -59,12 +71,24 @@ describe('watcher integration med vMix API', () => {
       )
       .reply(200, { ok: true });
 
-    // Act: Opret filen
-    await fs.writeFile(filePath, 'mockdata');
 
     // Giv chokidar/axios lidt tid til at reagere
     await sleep(500);
 
     // Selve verifikationen sker i afterEach via nock.isDone()
   });
+
+  test('Expect to return false when extension is not supported', async () => {
+    const filePath = path.join(dir, 'test.doesnotexist');
+    const ext = path.extname(filePath).toLowerCase();
+    expect(watcherHandle.config).toBe(false)
+    // expect(watcherHandle.config.supportedExtensions.includes(ext)).toBe(false)
+  });
+
+    test('Expect to return true when extension is', async () => {
+    const filePath = path.join(dir, 'test.mp4');
+
+    expect(watcherHandle.config.supportedExtensions.includes(ext)).toBe(true)
+  });
+
 });
